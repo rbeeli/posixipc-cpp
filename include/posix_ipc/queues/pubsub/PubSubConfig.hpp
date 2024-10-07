fa9d417a7d936633e4eb9214919702396bee6a26
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-10-08 01:48:21 +0200

    removed pubsubconfig parser

--- include/posix_ipc/queues/pubsub/PubSubConfig.hpp
+++ include/posix_ipc/queues/pubsub/PubSubConfig.hpp
@@ -3,7 +3,6 @@
 #include <format>
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
 #include "enums.hpp"
 
@@ -36,19 +35,6 @@ struct PubSubConfig
         );
     }
 };
-
-// PubSubConfig PubSubConfig::from_cfg_file(const string path)
-// {
-//     std::unordered_map<string, string> cfg_map = cfg::read_file(path);
-
-//     string shm_name = cfg_map["shm_name"];
-//     string capacity_str = cfg_map["capacity"];
-//     uint64 capacity = units::size_str_to_bytes<uint64>(capacity_str);
-//     auto queue_full_policy = QueueFullPolicy_from_string(cfg_map["queue_full_policy"]);
-//     bool log_message_drop = strings::as<bool>(cfg_map["log_message_drop"]);
-
-//     return {shm_name, capacity, queue_full_policy, log_message_drop};
-// }
 } // namespace pubsub
 } // namespace queues
 } // namespace posix_ipc
